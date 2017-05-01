242e4261267dc1fd5cae59efbf43e8e9ea008623
gunni1 <gunni1111@gmail.com>
2017-05-01 12:33:25 +0200

    Repo test hinzugefügt

--- build.sbt
+++ build.sbt
@@ -10,4 +10,9 @@ libraryDependencies ++= Seq( jdbc , cache , ws   , specs2 % Test )
 
 unmanagedResourceDirectories in Test <+=  baseDirectory ( _ /"target/web/public/test" )  
 
-resolvers += "scalaz-bintray" at "https://dl.bintray.com/scalaz/releases"  
+resolvers += "scalaz-bintray" at "https://dl.bintray.com/scalaz/releases"
+
+//Dependencies
+libraryDependencies += "org.scalactic" %% "scalactic" % "3.0.1"
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.1" % "test"
---
+++ test/avatar/MemoryAvatarRepositoryTest.scala
@@ -0,0 +1,35 @@
+package avatar
+
+package avatar
+
+import backend.avatar.{AvatarId, MemoryAvatarRepository}
+import org.scalatest.{FlatSpec, Matchers}
+
+/**
+  * Created by gunni on 21.03.17.
+  */
+class MemoryAvatarRepositoryTest extends FlatSpec with Matchers{
+  "A AvatarRepository" should "create two avatars with different ids" in {
+    val avatarRepository = new MemoryAvatarRepository()
+
+    val idOfHans = avatarRepository.createAvatar("Hans")
+    val idOfFrank = avatarRepository.createAvatar("Frank")
+    idOfHans should equal (AvatarId("1"))
+    idOfFrank should equal (AvatarId("2"))
+  }
+
+  it should "supply a created avatar by given id" in {
+    val avatarRepository = new MemoryAvatarRepository()
+    val idOfHans = avatarRepository.createAvatar("Hans")
+    val maybeHans = avatarRepository.getAvatar(idOfHans)
+
+    maybeHans.isDefined should be (true)
+    maybeHans.get.name should be ("Hans")
+  }
+
+  it should "supply a empty option when asked for a unset id" in {
+    val maybeAvatar = new MemoryAvatarRepository().getAvatar(AvatarId("1"))
+    maybeAvatar.isDefined should be (false)
+  }
+}
+
