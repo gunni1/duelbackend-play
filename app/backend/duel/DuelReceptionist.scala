f07bf6583e7d0a5f2a4c571dfed54141246ef0a5
gunni1 <gunni1111@gmail.com>
2017-06-06 23:32:39 +0200

    Experimente Promise

--- app/backend/duel/DuelReceptionist.scala
+++ app/backend/duel/DuelReceptionist.scala
@@ -1,10 +1,15 @@
 package backend.duel
 
 import backend.avatar.Avatar
-import backend.duel.dto.RequestDuelResponse
+import backend.duel.dto.{DuelRequestTimedOut, DuelStarted, RequestDuelResponse}
+import backend.duel.persistence.DuelId
 
-import scala.concurrent.{Future}
+import scala.collection.concurrent
+import scala.collection.concurrent.TrieMap
+import scala.concurrent.{Await, Future, Promise}
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.{Failure, Success}
+import scala.concurrent.duration._
 
 /**
   * - Koordiniert Anfragen zu Duellen.
@@ -12,6 +17,8 @@ import scala.concurrent.ExecutionContext.Implicits.global
   * - Nur wenn beide Spieler einem Duell zustimmen wird es gestartet.
   */
 class DuelReceptionist {
+  private val openRequests: concurrent.TrieMap[(Avatar, Avatar), Promise[RequestDuelResponse]] =
+    new TrieMap[(Avatar, Avatar), Promise[RequestDuelResponse]]()
 
   /**
     * Synchronisiert den Duellstart. Es wird ein wartender Request bestätigt oder auf die Bestätigung eines Requests
@@ -23,14 +30,28 @@ class DuelReceptionist {
     */
   def requestDuel(own: Avatar, other: Avatar): RequestDuelResponse = {
 
-    val response = Future {
-      Thread.sleep(10000)
+    if(openRequests contains (other -> own)){
+      //Dummy
+      val duelId = DuelId("2")
+      DuelStarted(duelId, 1)
+
+      //Promise complete
+      openRequests( other -> own).success(DuelStarted(duelId, 1))
+      DuelStarted(duelId, 1)
     }
+    else{
+      val requestTupel = (own -> other)
+      val p = Promise[RequestDuelResponse]
 
+      openRequests += requestTupel -> p
 
+      val future = p.future
 
-
-    ???
+      future.onComplete({
+        case Success(duelStarted) => openRequests.remove((own -> other)); duelStarted
+        case Failure(_) => DuelRequestTimedOut
+      })
+      Await.result(future, 30 seconds)
+    }
   }
-
 }
