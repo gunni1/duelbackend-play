eca2045f540361aa88e734e919bccea441103f3c
gunni1 <gunni1111@gmail.com>
2017-06-19 22:44:02 +0200

    #5 Protokoll-Events die zweite

---
+++ app/backend/duel/DuelEvent.scala
@@ -0,0 +1,21 @@
+package backend.duel
+
+import backend.avatar.persistence.AvatarId
+/**
+  * Repräsentiert ein Ereignis in einem Duell:  Jedes Event besitzt
+  *   - DuelId
+  *   - EventId (fortlaufend wärend des Duells)
+  */
+trait DuelEvent
+case class ActionEvent() extends DuelEvent
+case class DuelFinishedEvent() extends DuelEvent
+/**
+  * Das Duell ist beendet. Ein Avatar hat regulär verloren und wird entfernt.
+  */
+case class AvatarLost(avatarId: AvatarId) extends DuelFinishedEvent
+
+/**
+  * Das Duell ist beendet. Ein Spieler hat aufgegeben und der
+  * unterlegene Avatar bleibt existent.
+  */
+case class ThrownInTheTowel(avatarId: AvatarId) extends DuelFinishedEvent
--- app/backend/duel/persistence/DuelRepository.scala
+++ app/backend/duel/persistence/DuelRepository.scala
@@ -1,5 +1,6 @@
 package backend.duel.persistence
 
+import backend.duel.DuelEvent
 import backend.simulation.DuelProtocol
 /**
   * Created by gunni on 09.05.17.
@@ -11,13 +12,22 @@ trait DuelRepository {
   def nextDuelId: DuelId
 
   /**
+    * Speichert ein Ereignis wärend eines Duells.
+    */
+  def saveEvent(duelEvent: DuelEvent)
+
+
+
+  /**
     * Speichert ein DuelProtocol unter einer bestimmten DuelId. Wirft IllegalArgumentException
     * falls die DuelId bereits vergeben sein sollte.
     */
+  @Deprecated
   def saveDuelProtocol(duelId: DuelId, duelProtocol: DuelProtocol): Unit
 
   /**
     * Liefert ein DuelProtocol anhand einer DuelId
     */
+  @Deprecated
   def getDuelProtocol(duelId: DuelId): Option[DuelProtocol]
 }
