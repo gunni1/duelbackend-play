07fcc059999b06b5b6d6d9120c650492536fd01b
gunni1 <gunni1111@gmail.com>
2017-07-18 22:34:10 +0200

    #9 ErsterTest grün

--- test/backend/simulation/DuelSimulatorSpec.scala
+++ test/backend/simulation/DuelSimulatorSpec.scala
@@ -3,13 +3,13 @@ package backend.simulation
 
 import java.time.ZonedDateTime
 
-import akka.actor.{ActorRef, ActorSystem, Props}
+import akka.actor.{ActorRef, ActorSystem, Props, Scheduler, Scope}
 import akka.testkit.{ImplicitSender, TestActorRef, TestActors, TestKit, TestProbe}
 import backend.avatar.Avatar
 import backend.avatar.persistence.AvatarId
 import backend.duel.AsyncExecutionTimeSetter.SetNextExecutionTime
 import backend.duel.DuelSimulator.InitiateDuelBetween
-import backend.duel.{AsyncExecutionTimeSetter, DuelSimulator}
+import backend.duel.{ActionExecutionTimeService, AsyncExecutionTimeSetter, DuelSimulator}
 import backend.duel.persistence.{DuelEventPersister, DuelId}
 import org.scalatest.mockito.MockitoSugar
 import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
@@ -26,34 +26,44 @@ class DuelSimulatorSpec extends TestKit(ActorSystem("DuelSimulatorSpec"))
     TestKit.shutdownActorSystem(system)
   }
 
-  def mockDuelPersister = mock[ActorRef]
+  def mockDuelEventPersister = mock[ActorRef]
 
   def mockDuelExecTimeSetter = mock[ActorRef]
 
+  def mockActionExecService = mock[ActionExecutionTimeService]
+
   def duelId = DuelId("1")
+
+
 
   "A DuelSimulator" must {
     "schedule the first action" in {
-      val duelSimulator = system.actorOf(Props(new DuelSimulator(mockDuelPersister,mockDuelExecTimeSetter,duelId)))
-      //val duelSimulator = TestActorRef(Props(new DuelSimulator(mockDuelPersister,mockDuelExecSetter,duelId)))
+      val mockScheduler = mock[Scheduler]
+      val duelSimulator = system.actorOf(Props(new DuelSimulator(mockDuelEventPersister,mockDuelExecTimeSetter,duelId)))
+
+
 
       val avatars = createAvatars
       duelSimulator ! InitiateDuelBetween(avatars._1,avatars._2)
 
       within(1 second){
-        //duelSimulator.
+        fail("unfertig")
       }
     }
 
     "set initial actiontime" in {
       val execTimeSetter = TestProbe()
-      val duelSimulator = TestActorRef(Props(new DuelSimulator(mockDuelPersister,execTimeSetter.ref,duelId)))
+      val duelSimulator = TestActorRef(Props(new DuelSimulator(mockDuelEventPersister,execTimeSetter.ref,duelId)))
 
       val avatars = createAvatars
       duelSimulator ! InitiateDuelBetween(avatars._1,avatars._2)
 
       within(1 second){
-        execTimeSetter.expectMsgClass(SetNextExecutionTime)
+        execTimeSetter.expectMsgPF(){
+          case SetNextExecutionTime(resultDuelId,_) => resultDuelId should be (duelId)
+          case _ => fail("falsches Event empfangen")
+        }
+
       }
     }
   }
